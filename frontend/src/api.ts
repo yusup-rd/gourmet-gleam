f475b21d3157c7a98028f600bfb92510d553312a
Yusup Rejebov <yusuprd@icloud.com>
2024-03-20 19:10:22 +0800

    Minor fix on searchTerm passing query if query is multiple words

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -2,7 +2,8 @@ import { Recipe } from "./types";
 
 export const searchRecipes = async (searchTerm: string, page: number) => {
     const baseUrl = new URL("http://localhost:5000/api/recipes/search");
-    baseUrl.searchParams.append("searchTerm", searchTerm);
+    const processedSearchTerm = searchTerm.replace(/\s+/g, "+");
+    baseUrl.searchParams.append("searchTerm", processedSearchTerm);
     baseUrl.searchParams.append("page", String(page));
 
     const response = await fetch(baseUrl);
